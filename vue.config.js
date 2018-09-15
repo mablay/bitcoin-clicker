6552e020aa2c56f0ce0dfada3e78031a9e023d27
Marc Ablay <marc.ablay@gmail.com>
2018-09-16 00:33:29 +0200

    github pages deployment attempt

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   pwa: {
-    name: 'bitcoin-clicker'
+    name: 'bitcoin-clicker',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'translucent',
+    themeColor: '#FF9900'
   }
 }
