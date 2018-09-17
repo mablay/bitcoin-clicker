98e048328dfbee0181251efa3bcd8092d45c63f3
Marc Ablay <marc.ablay@gmail.com>
2018-09-17 22:00:53 +0200

    set stricter linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    // 'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   rules: {
