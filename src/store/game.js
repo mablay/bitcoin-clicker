6d8b85e7c454fd51b88a46c085f358d2546833f2
Marc Ablay <marc.ablay@gmail.com>
2018-09-18 21:03:07 +0200

    set default theme to dark

--- src/store/game.js
+++ src/store/game.js
@@ -1,8 +1,22 @@
 const game = {
   state: {
+    /**
+     * game speed
+     * ----------
+     * The speed factor should state how much faster
+     * the game time elapses in comparison with the real
+     * world.
+     * speed: 1 => game and reality progress evenly.
+     *          => blocktime 600 seconds
+     * speed: 100k => 1 game-year elapses in ~5 minutes.
+     *             => blocktime 6ms
+     *
+     * TODO: game.speed requires a meaningful unit.
+     *       At the moment the unit of speed is meaningless.
+     */
     speed: 10,
     history: [],
-    theme: 'light'
+    theme: 'dark'
   },
   mutations: {
     log: (state, msg) => {
