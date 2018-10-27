67c047b8e9f781f2dd4048d258068da37466cc74
Marc Ablay <marc.ablay@gmail.com>
2018-10-27 14:07:08 +0200

    deploy

---
+++ precache-manifest.f3824f63e6ce7fd57bef6fb66277e07a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "254f089c28619a07898c483085dd13df",
+    "url": "/bitcoin-clicker/index.html"
+  },
+  {
+    "revision": "68dc4f8ee38a41214b165401f276fb46",
+    "url": "/bitcoin-clicker/img/bitcoin-clicker-v0.3.5.gif"
+  },
+  {
+    "revision": "4ae92f135136d4528b9dd336ff821490",
+    "url": "/bitcoin-clicker/img/screenshot-v0.3.1.png"
+  },
+  {
+    "revision": "413f844a2b2edf069a82",
+    "url": "/bitcoin-clicker/js/chunk-2d838918.2e6df0ce.js"
+  },
+  {
+    "revision": "cb98a715ce24e4adad7c8cf2ac33570b",
+    "url": "/bitcoin-clicker/img/splash-2048.jpeg"
+  },
+  {
+    "revision": "c3f04a8e4cf3a99f6efd",
+    "url": "/bitcoin-clicker/js/chunk-vendors.b655ab15.js"
+  },
+  {
+    "revision": "e7653afce59af4ba7388281836e88e3f",
+    "url": "/bitcoin-clicker/img/splash-1300.jpeg"
+  },
+  {
+    "revision": "3a85873c7e8901e0f2b4",
+    "url": "/bitcoin-clicker/js/settings.02327b95.js"
+  },
+  {
+    "revision": "2534a3c581543b50d58c",
+    "url": "/bitcoin-clicker/js/app.45755b84.js"
+  },
+  {
+    "revision": "5e0bd1c281a62a380d7a948085bfe2d1",
+    "url": "/bitcoin-clicker/robots.txt"
+  },
+  {
+    "revision": "5efec115c0e0d18b17338c073603377b",
+    "url": "/bitcoin-clicker/googleb50f83a6279eb35d.html"
+  },
+  {
+    "revision": "3a85873c7e8901e0f2b4",
+    "url": "/bitcoin-clicker/css/settings.70a13161.css"
+  },
+  {
+    "revision": "c3f04a8e4cf3a99f6efd",
+    "url": "/bitcoin-clicker/css/chunk-vendors.f04610e2.css"
+  },
+  {
+    "revision": "413f844a2b2edf069a82",
+    "url": "/bitcoin-clicker/css/chunk-2d838918.defd6dce.css"
+  },
+  {
+    "revision": "2534a3c581543b50d58c",
+    "url": "/bitcoin-clicker/css/app.0a146397.css"
+  },
+  {
+    "revision": "a0890234c114cc0dc1c84ef5b333514f",
+    "url": "/bitcoin-clicker/audio/click.mp3"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+
+importScripts(
+  "/bitcoin-clicker/precache-manifest.f3824f63e6ce7fd57bef6fb66277e07a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "bitcoin-clicker"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
